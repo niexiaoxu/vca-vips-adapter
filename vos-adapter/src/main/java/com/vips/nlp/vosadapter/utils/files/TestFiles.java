13a238c273e7eaf9d4583a3a7ebf12593cdf247e
niexiaoxu <niexiaoxu@foxmail.com>
2018-09-17 02:39:54 +0800

    提取工具类

--- vos-adapter/src/main/java/com/vips/nlp/vosadapter/utils/files/TestFiles.java
+++ vos-adapter/src/main/java/com/vips/nlp/vosadapter/utils/files/TestFiles.java
@@ -1,16 +1,20 @@
 package com.vips.nlp.vosadapter.utils.files;
 
+import com.vips.nlp.vosadapter.utils.validate.CheckValue;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 
 @Log4j2
 public class TestFiles {
+
+    public static final LinkedHashSet<String> sourceSet = new LinkedHashSet<>();
+    public static final LinkedHashSet<String> targetSet = new LinkedHashSet<>();
+
     public static void main(String[] args) throws InterruptedException {
-//        readAndWrite("/test/sjp2.csv", "/test/sjpt_2.csv");
+//        readAndFormatWrite("/test/sjp2.csv", "/test/sjpt_2.csv");
 //        Thread.sleep(15000);
         readAndWrite("/test/sjpt_2.csv", "/test/mid_all_parse.csv", "/test/diff.csv");
     }
@@ -21,7 +25,7 @@ public class TestFiles {
      * @param readFilePath  读取文件路径
      * @param writeFilePath 写入文件路径
      */
-    public static void readAndWrite(String readFilePath, String writeFilePath) {
+    public static void readAndFormatWrite(String readFilePath, String writeFilePath) {
         String encoding = "UTF-8";
         File readFile = new File(readFilePath);
         if (!checkReadFile(readFile)) {
@@ -47,8 +51,52 @@ public class TestFiles {
         }
     }
 
+
     /**
-     * 查看文件可读性
+     * 比较并记录源文件与目标文件的差异部分
+     *
+     * @param sourceFilePath 源文件
+     * @param targetFilePath 目标文件
+     * @param diffFilePath   差异文件
+     */
+    public static void readAndWrite(String sourceFilePath, String targetFilePath, String diffFilePath) {
+        long start = System.currentTimeMillis();
+        String encoding = "UTF-8";
+        int containsCount = 0;
+        //write
+        File writeFile = new File(diffFilePath);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(writeFile);
+             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, encoding);
+             BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter)) {
+            getLinkedHashSet(sourceSet, sourceFilePath, encoding);
+            getLinkedHashSet(targetSet, targetFilePath, encoding);
+            int scount = 0;
+            for (String smid : sourceSet) {
+                scount++;
+                int tcount = 0;
+                for (String tmid : targetSet) {
+                    tcount++;
+                    if (smid.equals(tmid)) {
+                        log.info("相同： mid：{}, 源行数：{}, 目标行数：{}", new Object[]{smid, scount, tcount});
+                        bufferedWriter.write(smid + "\r\n");
+                        bufferedWriter.flush();
+                        containsCount++;
+                        continue;
+                    } else {
+                        log.warn("差异： mid：{}, 源行数：{}, 目标行数：{}", new Object[]{smid, scount, tcount});
+                    }
+                }
+            }
+            long end = System.currentTimeMillis();
+            log.info("执行完毕，耗时：{}, 相同总数：{}", new Object[]{containsCount, (end - start)});
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("读写失败，错误信息：{}", e.getMessage());
+        }
+    }
+
+    /**
+     * 查看文件并检验可读性
      *
      * @param file
      * @return
@@ -63,91 +111,36 @@ public class TestFiles {
         return result;
     }
 
-
     /**
-     * 比较并记录源文件与目标文件的差异部分
+     * 指定文件内容转linkedHashSet
      *
-     * @param sourceFilePath 源文件
-     * @param targetFilePath 目标文件
-     * @param diffFilePath   差异文件
+     * @param filePath 文件路径
+     * @return
      */
-    public static void readAndWrite(String sourceFilePath, String targetFilePath, String diffFilePath) {
-        long start = System.currentTimeMillis();
-        String encoding = "UTF-8";
-        File sourceFile = new File(sourceFilePath);
-        File targetFile = new File(targetFilePath);
-        if (!checkReadFile(sourceFile) || !checkReadFile(targetFile)) {
-            return;
+    public static LinkedHashSet<String> getLinkedHashSet(LinkedHashSet<String> linkedHashSet, String filePath, String encoding) {
+        File file = new File(filePath);
+        if (!checkReadFile(file)) {
+            return null;
         }
-        //init read
-        FileInputStream sourcefileInputStream = null;
-        InputStreamReader sourceinputStreamReader = null;
-        BufferedReader sourcebufferedReader = null;
-        //init write
-        FileOutputStream fileOutputStream = null;
-        OutputStreamWriter outputStreamWriter = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            //source
-            sourcefileInputStream = new FileInputStream(sourceFile);
-            sourceinputStreamReader = new InputStreamReader(sourcefileInputStream, encoding);
-            sourcebufferedReader = new BufferedReader(sourceinputStreamReader);
-            String slineTxt = null;
-            LinkedHashSet<String> sourceSet = new LinkedHashSet<>();
-            while ((slineTxt = sourcebufferedReader.readLine()) != null) {
-                sourceSet.add(slineTxt);
-            }
-            //target
-            LinkedHashSet<String> targetSet = new LinkedHashSet<>();
-            sourcefileInputStream = new FileInputStream(targetFile);
-            sourceinputStreamReader = new InputStreamReader(sourcefileInputStream, encoding);
-            sourcebufferedReader = new BufferedReader(sourceinputStreamReader);
-            while ((slineTxt = sourcebufferedReader.readLine()) != null) {
-                targetSet.add(slineTxt);
-            }
-            //write
-            File writeFile = new File(diffFilePath);
-            fileOutputStream = new FileOutputStream(writeFile);
-            outputStreamWriter = new OutputStreamWriter(fileOutputStream, encoding);
-            bufferedWriter = new BufferedWriter(outputStreamWriter);
-            int scount = 0;
-            for (String smid : sourceSet) {
-                scount++;
-                int tcount = 0;
-                for (String tmid : targetSet) {
-                    tcount++;
-                    if (smid.equals(tmid)) {
-                        log.info("相同： mid：{}, 源行数：{}, 目标行数：{}", new Object[]{smid, scount, tcount});
-                        bufferedWriter.write(smid + "\r\n");
-                        bufferedWriter.flush();
-                        continue;
-                    } else {
-                        log.warn("差异： mid：{}, 源行数：{}, 目标行数：{}", new Object[]{smid, scount, tcount});
-                    }
-                }
+        if (linkedHashSet == null) {
+            linkedHashSet = new LinkedHashSet<>();
+        }
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, encoding);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+            String lineContent = null;
+            while ((lineContent = bufferedReader.readLine()) != null) {
+                linkedHashSet.add(lineContent);
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            log.error("读写失败，错误信息：{}", e.getMessage());
-        } finally {
-            try {
-                bufferedWriter.close();
-                outputStreamWriter.close();
-                fileOutputStream.close();
-                sourcebufferedReader.close();
-                sourceinputStreamReader.close();
-                sourcefileInputStream.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-                log.error("关闭流失败，错误信息：{}", e.getMessage());
-            }
+            log.error("读取文件转LinkedHashSet异常，错误信息：{}", e.getMessage());
         }
-        long end = System.currentTimeMillis();
-        log.info("执行完毕，耗时：{}", end - start);
+        return linkedHashSet;
     }
 
     /**
      * 去重
+     *
      * @param list
      */
     private static void removeDuplicate(List<String> list) {
